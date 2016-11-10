70bdf66b964f014f7927b3fecfb6672fcded52b8
Sphar-io <jonathan@sphar.io>
2016-11-10 14:58:47 -0500

    Vector set-up

--- birb.js
+++
@@ -1,6 +0,0 @@
-function birb(xi, yi, vxi, vyi){
-	this.x = xi;
-	this.y = yi;
-	this.vx = vxi;
-	this.vy = vyi;
-}	
--- vector.js
+++ vector.js
@@ -1,4 +1,27 @@
 function vector(xi, yi){
 	this.x = xi;
 	this.y = yi;
+
+	this.add = function(vector){
+			this.x += vector.x;
+			this.y += vector.y;
+	}
+	this.subtract = function(vector){
+			this.x -= vector.x;
+			this.y -= vector.y;
+	}
+	this.divide = function(num){
+			this.x /= num;
+			this.y /= num;
+	}
+
+	this.getLength = function(){
+		return Math.sqrt(this.x*this.x+this.y*this.y);
+	}
+	this.normalize = function(){
+		var mag = this.getLength;
+		if(mag > 0){
+			this.divide(mag);
+		}
+	}
 }
